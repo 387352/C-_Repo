3f21812899acb76bc5d966914b53e9d0aef5d9d4
387352 <49751351+387352@users.noreply.github.com>
2020-02-05 14:48:03 +0530

    Create firstnotrepetchar.py

---
+++ firstnotrepetchar.py
@@ -0,0 +1,29 @@
+from collections import *
+
+s = "qwewwwwwweeerrreedgggheetdfdfl"
+
+d = Counter(s)
+
+print("d:",d)
+
+for k,v in d.items():
+    if(v == 1):
+        print("first non repeting character is :",k)
+        break
+
+d1 = {}
+count = 0
+for i in s:
+    if(d1.get(i) == None ):
+        count = 1
+        d1[i] = count
+    else:
+        count = count+1
+        d1[i] = count
+
+print("d1:",d1)
+
+for k,v in d1.items():
+    if(v == 1):
+        print("first non repeting character is :",k)
+        break
